31bbd8310200d97b1ca8ac7fe49369cb3f3c6fd2
The Emeryverse <server@emeryverse.com>
2024-11-27 00:03:34 -0600

    bug in line amount checker

--- testReadFile.sh
+++ testReadFile.sh
@@ -12,11 +12,13 @@ arrayLineCtr=1
 lineCtr=1
 totalLines=$(wc -l < $file | xargs)
 
+
+echo $(($totalLines % 3 ==0)) # Line amount checker still bugged
 if (($totalLines % 3 ==0))
 then
     printf "File properly configured."
 else
-    printf "\n-----ERROR-----\n\nYour 'containerConfigurations.txt' file is missing lines. ALL lines must be present, EVEN EMPTY ONES!!\n\nLine 1: Container Name\nLine 2: Run command parameters, such as -e, -p, etc. that are not already included in your custom flags variable.\nLine 3: Image repo, such as docker.io/hello-world\n\nLines 1 and 2 can be left blank, the program will auto-generate a name for you if you don't have one (although it\nis highly recommended you make your own, easy to remember and type one).\n\n***If line 3 is blank the program will exit and give an error,\nas an image repo is required to run a docker container.***\n"
+    printf "\n-----ERROR-----\n\nYour 'containerConfigurations.txt' file is missing lines.\nALL lines must be present, EVEN EMPTY ONES!!\n\nLine 1: Container Name\nLine 2: Run command parameters, such as -e, -p, etc. that are not already included in your custom flags variable.\nLine 3: Image repo, such as docker.io/hello-world\n\nLines 1 and 2 can be left blank, the program will auto-generate a name for you if you don't have one (although it\nis highly recommended you make your own, easy to remember and type one).\n\n***If line 3 is blank the program will exit and give an error,\nas an image repo is required to run a docker container.***\n"
     printf "\n\nExample config file layout:\n\nLine 1 (container name): helloWorld\nLine 2 (run parameters): -e TZ=America/Chicago -p 1111:1111 -v /path/to/data:/data\nLine 3 (image repo/location): docker.io/hello-world\nLine 4 (next container's name): myContainer\nLine 5: -e TZ=America/NewYork -p 3333:33 -e var=value --network=host -v /path/to/my/data/config:/config\nLine 6: ghcr.io/myAwesomeImageRepo\nLine 7: (OK to leave empty, program will auto-generate name.)\nLine 8: (Also ok to leave empty, if you just need the custom flags used.)\nLine 9: docker.io/image (Never leave empty, will cause Docker to error.)\netc...\n\n"
     printf "\nTo see an example configuration file, please visit the github repo.\n"
     exit 1
